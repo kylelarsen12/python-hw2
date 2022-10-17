0fd5b71b3944ac7e6389d02f3e7684e44dedb4a7
unknown <kylelarsen12@gmail.com>
2022-10-17 18:41:32 -0400

    Complete activity

--- function_fun4.py
+++ function_fun4.py
@@ -31,3 +31,39 @@ def num_within(test_num, min, max):
                 return False
 
 
+#pascal's triangle
+triangle = [[1], [1,1]]
+def pascal(num_rows):
+        if num_rows < 1:
+                print("Select 1 or more rows please")
+        elif num_rows == 1:
+                print(triangle[0])
+        else:
+                row_number = 2
+                while len(triangle) < num_rows:
+                        row = []
+                        previous_row = triangle[row_number - 1]
+                        row_length = len(previous_row) + 1
+                        for individual_row_number in range(row_length):
+                                if individual_row_number == 0:
+                                        row.append(1)
+                                elif individual_row_number > 0 and individual_row_number < row_length - 1:
+                                        row.append(triangle[row_number-1][individual_row_number-1]+triangle[row_number-1][individual_row_number])
+                                else:
+                                        row.append(1) 
+                        triangle.append(row)
+                        row_number += 1
+                for row in triangle:
+                        print(row)
+
+#tests
+print(max_num(1, 2, 3))
+print(max_num(18, 25031831, 4))
+print(mult_list([2, 3, 4]))
+print(mult_list([5, 5, 10]))
+print(rev_string("Kyle"))
+print(rev_string("Peanut butter and jelly"))
+print(num_within(50, 1, 10))
+print(num_within(5, 1, 100))
+pascal(5)
+pascal(7)
